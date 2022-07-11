ca46214544faebfbfd6c648d1999f204082c10fd
Udeani <udeani.henry@gmail.com>
2022-07-11 02:04:09 -0700

    fixing erors

--- _printf.c
+++ _printf.c
@@ -1,3 +1,8 @@
+/*
+ * File: _printf.c
+ * Auth: Udeani henry
+ */
+
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,6 +12,12 @@
 void string_s(va_list arg);
 void char_c(va_list arg);
 
+/**
+ * _printf - function to print any buffer to standard output
+ * @format: pointer to array of characters to print
+ * ...: variadic functions to handle options for the print
+ * Return: 0
+ */
 int _printf(const char *format, ...)
 {
 	int n, i = 0;
@@ -47,6 +58,10 @@ int _printf(const char *format, ...)
 	return(0);
 }
 
+/*
+ * string_s - function to prints an array of characters.
+ * @arg: variadic list to print
+ */
 void string_s(va_list arg)
 {
 	long unsigned int j = 0;
@@ -56,6 +71,10 @@ void string_s(va_list arg)
 		(write(1, &v[j], 1));
 }
 
+/*
+ * char_c - function to print single characters
+ * @arg: variadic list to print.
+ */
 void char_c(va_list arg)
 {
 	long unsigned int j = 0;
